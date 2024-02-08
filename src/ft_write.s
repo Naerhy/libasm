802e19d9bbeef82525b396e1bc8589c1274b8116
Naerhy <naerhy@protonmail.com>
2024-02-08 16:47:10 +0100

    Set errno on syscall error (ft_write)

--- src/ft_write.s
+++ src/ft_write.s
@@ -1,7 +1,18 @@
 section .text
 	global ft_write
+	extern __errno_location
 
 	ft_write:
 		mov rax, 1
 		syscall
+		cmp rax, 0
+		jge .done
+
+	.error:
+		mov rbx, rax
+		call __errno_location wrt ..plt
+		mov [rax], rbx
+		mov rax, -1
+
+	.done:
 		ret
